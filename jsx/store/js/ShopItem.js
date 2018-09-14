4178cb0d634d6720a66cec5903d15c6e72208e26
Jimka92 <36789491+Jimka92@users.noreply.github.com>
2018-09-14 21:54:52 +0400

    Update ShopItem.js

--- jsx/store/js/ShopItem.js
+++ jsx/store/js/ShopItem.js
@@ -1,19 +1,21 @@
-const showItem = function (props) {
-const {brand, title, description, descriptionFull, price, currency} = props.item
-return(
-  <div className="main-content">
-  <h2>{brand}</h2>
-  <h1>{title}</h1>
-  <h3>{description}</h3>
-  <div className="description">{descriptionFull}</div>
-  <div className="highlight-window  mobile">
-   <div class="highlight-overlay"></div>
-  <div className="divider"></div>
-  <div className="purchase-info">
-    <div class="price">{currency + price.toFixed(2)}</div>
-    <button>Добавить в корзину</button>
-  </div>
-</div>
-)
+const ShopItem = function (props) {
+  const {brand, title, description, descriptionFull,
+        price, currency} = props.item;
+  return(
+      <div className="main-content">
+          <h2>{brand}</h2>
+          <h1>{title}</h1>
+          <h3>{description}</h3>
+          <div className="description">{descriptionFull}</div>
+          <div className="highlight-window  mobile">
+              <div class="highlight-overlay"></div>
+          </div>
+          <div className="divider"></div>
+          <div className="purchase-info">
+              <div className="price">{currency + price.toFixed(2)}</div>
+              <button>Добавить в корзину</button>
+          </div>
+      </div>
+  )
 };
   
