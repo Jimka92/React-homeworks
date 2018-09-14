e2e1ea4a2bfe52e993c91e63844916afe016adf6
Jimka92 <36789491+Jimka92@users.noreply.github.com>
2018-09-14 22:23:29 +0400

    Update Menu.js

--- jsx/menu/js/Menu.js
+++ jsx/menu/js/Menu.js
@@ -1,16 +1,16 @@
-const Menu = ({items, opened}) {
-  const dropDownmenu = items.map( (item, index) => <li key={index}><a href={item.href}>{item.title}</a></li> );
-  return(
-    <div className={"menu" + (opened ? " menu-open " : "" )}>
-      <div className="menu-toggle"><span></span></div>
-        {opened && (
-         <nav>
-           <ul>
-            {dropDownmenu}
-           </ul>
-         </nav>
-      )}
-   </div>
+const Menu = function ({items, opened}) {
+    const dropDownmenu = items.map( (item, index) => <li key={index}><a href={item.href}>{item.title}</a></li> );
+    return(
+      <div className={"menu" + (opened ? " menu-open" : "")}>
+          <div className="menu-toggle"><span></span></div>
+          {opened && (
+              <nav>
+                  <ul>
+                      {dropDownmenu}
+                  </ul>
+              </nav>
+          )}
+      </div>
   );
 };
 
