d7400d35f6e99ba03fad1e4f2ce5649b92bfd36c
Jimka92 <36789491+Jimka92@users.noreply.github.com>
2018-09-14 22:02:05 +0400

    Update ShopItem.js

--- jsx/store/js/ShopItem.js
+++ jsx/store/js/ShopItem.js
@@ -1,21 +1,20 @@
 const ShopItem = function (props) {
-  const {brand, title, description, descriptionFull,
-        price, currency} = props.item;
-  return(
+  const {brand, title, description, descriptionFull, price, currency} = props.item;
+   return(
       <div className="main-content">
-          <h2>{brand}</h2>
-          <h1>{title}</h1>
-          <h3>{description}</h3>
-          <div className="description">{descriptionFull}</div>
-          <div className="highlight-window  mobile">
-              <div class="highlight-overlay"></div>
-          </div>
-          <div className="divider"></div>
-          <div className="purchase-info">
-              <div className="price">{currency + price.toFixed(2)}</div>
-              <button>Добавить в корзину</button>
-          </div>
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window  mobile">
+        <div class="highlight-overlay"></div>
       </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+      <div className="price">{currency + price.toFixed(2)}</div>
+      <button>Добавить в корзину</button>
+      </div>
+    </div>
   )
 };
   
