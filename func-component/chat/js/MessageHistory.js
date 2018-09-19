3cfa70ac17a44278621321d5987e10f496ae22f3
Jimka92 <36789491+Jimka92@users.noreply.github.com>
2018-09-19 14:33:46 +0300

    Update MessageHistory.js

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -2,3 +2,23 @@
 
 function MessageHistory(props) {
   if (!Array.isArray(props.list) || props.list.length < 1) return;
+  
+  const message = props.list.map(function (message) {
+    // Switch Case для определения типа испольуемого компонента
+    let usedComponent;
+    switch(message.type){
+      case "message":
+        usedComponent = message;
+        break;
+        case 'response':
+                UsedComponent = Response;
+                break;
+            case 'typing':
+                UsedComponent = Typing;
+                break;
+            default:
+                return null;
+        }
+    
+        
+    
