a5984770c842c2e41a926cf91641c20362e19f89
Jimka92 <36789491+Jimka92@users.noreply.github.com>
2018-09-19 14:53:59 +0300

    Update app.js

--- func-component/listing/js/app.js
+++ func-component/listing/js/app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function webStore(bid) {
+  console.log(bid);
+}
+
+const siteurl = 'https://neto-api.herokuapp.com/etsy';
+
+// Создадим компонент Listing 
+function Listing(props) {
+  if (!props.items || !Array.isArray(props.items) || !props.items.length) return;
+  
+                 
+
