de622ce68eb8a2873648116b98ed6625ff76334f
Jimka92 <36789491+Jimka92@users.noreply.github.com>
2018-09-25 22:41:36 +0400

    Update App.js

--- state/layouts/js/App.js
+++ state/layouts/js/App.js
@@ -4,15 +4,46 @@ const VIEW_LIST = "view_list";
 const VIEW_MODULE = "view_module";
 
 class App extends React.Component {
+  constructor(props){
+   super(props);
+    
+    if (!localStorage.getItem('cards_layout')) {
+      localStorage.setItem('cards_layout', VIEW_MODULE);
+    }
+    this.state = {
+      'cards_layout': localStorage.getItem('cards_layout')
+    };
+  }
+  
+  switchState(){
+    if (localStorage.getItem('cards_layout') === VIEW_LIST) {
+      localStorage.setItem('cards_layout', VIEW_MODULE);
+      this.setState({
+        'cards_layout': VIEW_MODULE
+      });
+      return VIEW_MODULE;
+    } else {
+      localStorage.setItem('cards_layout', VIEW_LIST);
+      this.setState({
+        'cards_layout': VIEW_LIST
+      });
+      return VIEW_LIST;
+    }
+  }
+    
+    
   render() {
     return (
       <div>
         <div className="toolbar">
           <IconSwitch
-            icon={VIEW_MODULE}
-            onSwitch={() => console.log("сменился тип вывода")} />
+            icon={this.state.cards_layout}
+            onSwitch={() => {
+              this.switchState();
+              console.log("сменился тип вывода");
+            }} />
         </div>
-        {this.renderLayout(true)}
+        {this.renderLayout(this.state.cards_layout === VIEW_MODULE)}
       </div>
     );
   }
