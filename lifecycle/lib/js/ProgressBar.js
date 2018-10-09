9ae41219e9387e982a795de64a9bc6af3685434c
Jimka92 <36789491+Jimka92@users.noreply.github.com>
2018-10-09 23:41:24 +0400

    Update ProgressBar.js

--- lifecycle/lib/js/ProgressBar.js
+++ lifecycle/lib/js/ProgressBar.js
@@ -1,4 +1,104 @@
+function percToRad(percentage){
+  const deg = percentage * 360;
+  return deg * Math.PI / 180;
+} 
+
 class ProgressBar extends React.Component {
+  constructor(props){
+  super(props);
+    
+    this.state = {
+      total: props.total,
+      comleted: props.completed
+     }
+  }
+  
+  getCanvasCenter(canvas) {
+    const bb = canvas.getBoundingClientRect();
+    return {
+      centerX: bb.width / 2,
+      centerY: bb.height / 2,
+    }
+  }
+  
+  setCanvasDims(canvas){
+    const bb = canvas.getBoundingClientRect();
+    canvas.height = bb.height;
+    canvas.width = bb.width;
+  }
+  
+  drawProgressBar(total, completed){
+    const canvasCenter = this.getCanvasCenter(this.canvas);
+
+    const arcAngle = percToRad(completed / total);
+
+    this.ctx.lineWidth = 7;
+
+    this.ctx.clearRect(
+      0,
+      0,
+      this.canvas.width,
+      this.canvas.height
+    );
+
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = '#4ca89a';
+    this.ctx.arc(
+      canvasCenter.centerX,
+      canvasCenter.centerY,
+      52,
+      0,
+      2 * Math.PI
+    );
+    this.ctx.stroke();
+
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = '#96d6f4';
+    this.ctx.arc(
+      canvasCenter.centerX,
+      canvasCenter.centerY,
+      45,
+      0,
+      arcAngle
+    );
+    this.ctx.stroke();
+
+    const text = ( (completed / total) * 100 ).toFixed(1) + '%';
+
+    this.ctx.textAlign = 'center';
+    this.ctx.textBaseline = 'middle';
+    this.ctx.font="20px Arial";
+    this.ctx.fillText(
+      text,
+      canvasCenter.centerX,
+      canvasCenter.centerY
+      )
+  }
+  
+  componentDidMounth(){
+    this.canvas = document.querySelector('#progressCanvas');
+    this.setCanvasDims(this.canvas);
+    this.ctx = this.canvas.getContext('2d');
+
+    this.drawProgressBar(this.state.total, this.state.completed);
+
+    window.addEventListener('resize', () => {
+      this.setCanvasDims(this.canvas);
+      this.drawProgressBar(this.state.total, this.state.completed);
+    })
+  }
+  
+  componentWillReceiveProps({total, completed}){
+    this.setState({
+      total,
+      completed
+    })
+  }
+
+  componentDidUpdate(){
+    this.drawProgressBar(this.state.total, this.state.completed);
+  }
+    
   render() {
     return (
       <canvas id="progressCanvas" className="progress" />
