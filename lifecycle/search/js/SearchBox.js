a989db44fc673982fb3a291f2062e24b41b41b7e
Jimka92 <36789491+Jimka92@users.noreply.github.com>
2018-10-09 23:20:53 +0400

    Update SearchBox.js

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -4,20 +4,23 @@ function getCoords(elem) {
   return {
     top: box.top + pageYOffset,
     left: box.left + pageXOffset
-    };
+   };
 }
 
 class SearchBox extends React.Component {
-
-  constructor(props) {
+   constructor(props) {
     super(props);
-    this.state = { fixed: false };
+    this.state = {fixed: false };
   }
 
   render() {
     return <SearchBoxView fixed={this.state.fixed} />
   }
 
+  componentDidMount() {
+    this.searchBox = document.querySelector('.search-box');
+    this.searchBoxInitialTop = getCoords(this.searchBox).top;
+    
   isFixed() {
     return undefined;
   }
